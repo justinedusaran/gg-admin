869285dabf25707029219129eb8e18295bbfe3d4
Justine Marie Dusaran <jdusaran05@gmail.com>
2024-03-31 21:45:12 +0800

    modified firebase code

--- src/data/firebase/firebase.jsx
+++ src/data/firebase/firebase.jsx
@@ -3,13 +3,8 @@ import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCMamClOkwHXPfWjdYa64R1AIAQbuuPw1k",
-  authDomain: "dp-gutterguard.firebaseapp.com",
   databaseURL:
     "https://dp-gutterguard-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "dp-gutterguard",
-  storageBucket: "dp-gutterguard.appspot.com",
-  messagingSenderId: "122381802302",
-  appId: "1:122381802302:web:17a80f74e4fb7aff55aa6f",
 };
 
 const initializeFirebase = () => {
